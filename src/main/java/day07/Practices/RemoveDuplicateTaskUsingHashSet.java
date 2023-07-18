af91c82c0210f8f2478a6f743bece7365686e32a
SARAN2004 <116723092+SARAN2004@users.noreply.github.com>
2023-07-18 19:52:16 +0530

    day07

---
+++ src/main/java/day07/Practices/RemoveDuplicateTaskUsingHashSet.java
@@ -0,0 +1,5 @@
+package day07.Practices;
+
+public class RemoveDuplicateTaskUsingHashSet {
+
+}
---
+++ src/main/java/day07/Practices/Task.java
@@ -0,0 +1,20 @@
+package day07.Practices;
+import java.util.*;
+public class Task {
+
+	public static void main(String args[]) {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(2);
+		numbers.add(5);
+		numbers.add(2);
+		numbers.add(8);
+		numbers.add(0);
+		numbers.add(2);
+		numbers.add(2);
+		numbers.add(2);
+		System.out.println("Before Removing : " + numbers);
+		HashSet<Integer> removed = new HashSet<Integer>(numbers);
+		System.out.println("After Removing : " + removed);
+		
+	}
+}
