5fdf84727fd1ea62f547644ba4d0f1d39d632d8c
SARAN2004 <116723092+SARAN2004@users.noreply.github.com>
2023-07-25 18:02:12 +0530

    day12

---
+++ src/main/java/day12/Practices/TaskDAOTest.java
@@ -0,0 +1,74 @@
+package day12.Practices;
+
+
+
+
+import org.junit.jupiter.api.*;
+
+
+import java.util.List;
+
+class TaskDAOTest {
+
+    private TaskDAO taskDAO;
+
+    @BeforeEach
+    void setUp() {
+        taskDAO = new TaskDAO();
+    }
+
+    @Test
+    void testCreateTask() {
+        Task task = new Task();
+        task.id = 1;
+        task.name = "Task";
+        task.status = "Process";
+
+        try {
+            taskDAO.createTask(task);
+        } catch (DAOException e) {
+            e.printStackTrace();
+            Assertions.fail("Failed to create task: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testUpdateTask() {
+        Task task = new Task();
+        task.id = 13;
+        task.name = "Create Task";
+        task.status = "Completed";
+
+        try {
+            taskDAO.updateTask(task);
+        } catch (DAOException e) {
+            e.printStackTrace();
+            Assertions.fail("Failed to update task: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testDeleteTask() {
+        int taskId = 13;
+
+        try {
+            taskDAO.deleteTask(taskId);
+        } catch (DAOException e) {
+            e.printStackTrace();
+            Assertions.fail("Failed to delete task: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testGetAllTasks() {
+        try {
+            List<Task> tasks = taskDAO.getAllTasks();
+            for (Task task : tasks) {
+                System.out.println(task.id + " - " + task.name + " - " + task.status);
+            }
+        } catch (DAOException e) {
+            e.printStackTrace();
+            Assertions.fail("Failed to get all tasks: " + e.getMessage());
+        }
+    }
+}
--- src/test/java/day04/practice/RectangleTest.java
+++
@@ -1,7 +0,0 @@
-package day04.practice;
-import day04.Practices.*;
-public class RectangleTest {
-    public static void main(String[] args) {
-       System.out.println("hi");
-    }
-}
